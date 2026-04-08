5b796b773b415862f2308a1fe8471d66423f1e38
gopal612 <gopal612@gmail.com>
2026-04-08 17:36:16 +0530

    added winston logger - showing nest logs in kibana

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+import { WinstonModule } from 'nest-winston';       // publish logs to http port; so logstash can receive them
+import * as winston from 'winston';
+
 import { Logging23Interceptor } from './utils23/others23/logging-interceptor';
 
 async function bootstrap23() {
@@ -7,4 +11,30 @@ async function bootstrap23() {
     app.useGlobalInterceptors(new Logging23Interceptor());        // using the interceptor globally
     await app.listen(process.env.PORT ?? 5566);
 }
-bootstrap23();
+bootstarp23_winstonLogger();
+
+async function bootstarp23_winstonLogger() {
+    const app = await NestFactory.create(AppModule, {
+        logger: WinstonModule.createLogger({
+            transports: [                
+                new winston.transports.Console({                    // 1. Output to Console (stdout)
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.colorize(),
+                        winston.format.printf(({ timestamp, level, message, context }) => {
+                            return `[nest23] ${timestamp} ${level}: [${context || 'App'}] ${message}`;
+                        }),
+                    ),
+                }),
+                new winston.transports.Http({               // 2. Output to Logstash (HTTP)
+                    host: 'localhost', // or the IP of your Logstash container
+                    port: 50001,        // ensure this matches your logstash.conf
+                    path: '/',
+                    format: winston.format.json(), // Logstash prefers JSON via HTTP
+                }),
+            ],
+        }),
+    });
+    app.useGlobalInterceptors(new Logging23Interceptor());
+    await app.listen(process.env.PORT ?? 5566);
+}
--- src/utils23/others23/logging-interceptor.ts
+++ src/utils23/others23/logging-interceptor.ts
@@ -1,15 +1,17 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class Logging23Interceptor implements NestInterceptor {
-    
+    private readonly logger = new Logger('HTTP');
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         const now = Date.now();
-        console.log('Before route handler...'); // Runs before the handler
+        // console.log('Before route handler...'); // Runs before the handler
+        const request = context.switchToHttp().getRequest();
+        const { method, url } = request;
         return next.handle().pipe(
-            tap(() => console.log(`After route handler... Request took ${Date.now() - now}ms`)), // Runs after the handler returns
+            tap(() => this.logger.log(`${method} ${url} - Success; Request took ${Date.now() - now}ms`))
         );
     }
 }
